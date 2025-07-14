d2bfbaaf93fee1d6baf02319876c2f7522bf075a
michael-130 <michaelalbert752@gmail.com>
2025-07-14 16:38:13 +0800

    Update _layout.tsx before pulling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,6 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-import { Alert, Button, Text, TextInput, View } from 'react-native';
 
 export default function RootLayout() {
   const [loaded] = useFonts({
@@ -11,18 +8,18 @@ export default function RootLayout() {
   });
 
   if (!loaded) {
-    // Async font loading only occurs in development.
     return null;
   }
 
   return (
-<>
+    <>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
-          <Stack.Screen name="cart_and_order_page/order" options={{ title: "Order" }} />
+        <Stack.Screen name="cart_and_order_page/order" options={{ title: "Order" }} />
+        <Stack.Screen name="applyhome/Detail" /> {/* Just name, no component */}
       </Stack>
       <StatusBar style="auto" />
-</>
+    </>
   );
 }
